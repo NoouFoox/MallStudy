42cdf43d4e5eac1e7a76ee11e92f80f6c68c806b
nougat233 <nougat_hx@qq.com>
2021-03-21 15:13:48 +0800

    'updata'

--- src/components/common/swiper/swiper.vue
+++ src/components/common/swiper/swiper.vue
@@ -44,127 +44,171 @@ export default {
       scrolling: false//是否滚动
     }
   },
-  //初始化完后操作mounted
   mounted: function () {
-    //1.操作DOm在前后添加slide
+    // 1.操作DOM, 在前后添加Slide
     setTimeout(() => {
-      this.handleDom()
-      //开启定时器
-      this.startTimer()
-    }, 100)
+      this.handleDom();
+
+      // 2.开启定时器
+      this.startTimer();
+    }, 200)
   },
   methods: {
-    //定时器操作
+    /**
+     * 定时器操作
+     */
     startTimer: function () {
       this.playTimer = window.setInterval(() => {
         this.currentIndex++;
-        this.scroolContent(-this.currentIndex * this.totalWidth);
+        this.scrollContent(-this.currentIndex * this.totalWidth);
       }, this.interval)
     },
     stopTimer: function () {
-      window.clearInterval(this.playTimer)
+      window.clearInterval(this.playTimer);
     },
-    // 滚动到正确位置
-    scroolContent: function (currentPosition) {
-      //0.设置正在滚动
-      this.scrolling -= true;
-      //1.开始滚动动画
-      this.swiperStyle.transaction = 'transform' + this.animDuration + 'ms';
-      //anvas setTransform() 方法重置变换矩阵
-      this.setTransform(currentPosition)
-      //3判断滚动位置
+
+    /**
+     * 滚动到正确的位置
+     */
+    scrollContent: function (currentPosition) {
+      // 0.设置正在滚动
+      this.scrolling = true;
+
+      // 1.开始滚动动画
+      this.swiperStyle.transition = 'transform ' + this.animDuration + 'ms';
+      this.setTransform(currentPosition);
+
+      // 2.判断滚动到的位置
       this.checkPosition();
-      //4滚动完成
+
+      // 4.滚动完成
       this.scrolling = false
     },
+
+    /**
+     * 校验正确的位置
+     */
     checkPosition: function () {
       window.setTimeout(() => {
-        this.swiperStyle.transaction = '0ms';
+        // 1.校验正确的位置
+        this.swiperStyle.transition = '0ms';
         if (this.currentIndex >= this.slideCount + 1) {
           this.currentIndex = 1;
           this.setTransform(-this.currentIndex * this.totalWidth);
         } else if (this.currentIndex <= 0) {
           this.currentIndex = this.slideCount;
-          this.setTransform(-this.currentIndex * this.totalWidth)
+          this.setTransform(-this.currentIndex * this.totalWidth);
         }
-        //结束后回调
-        this.$emit("transitionEnd", this.currentIndex - 1)
+
+        // 2.结束移动后的回调
+        this.$emit('transitionEnd', this.currentIndex - 1);
       }, this.animDuration)
     },
-    //设置滚动位置
+
+    /**
+     * 设置滚动的位置
+     */
     setTransform: function (position) {
-      this.swiperStyle.transaction = `translate3d(${position}px,0,0)`;
-      //暂时不做兼容
+      this.swiperStyle.transform = `translate3d(${position}px, 0, 0)`;
+      this.swiperStyle['-webkit-transform'] = `translate3d(${position}px), 0, 0`;
+      this.swiperStyle['-ms-transform'] = `translate3d(${position}px), 0, 0`;
     },
-    //操作DOM在dom前后添加slide
+
+    /**
+     * 操作DOM, 在DOM前后添加Slide
+     */
     handleDom: function () {
+      // 1.获取要操作的元素
       let swiperEl = document.querySelector('.swiper');
-      //获取所有指定类名的元素
-      let slidesEls = swiperEl.getElementsByClassName('slide')
-      //保存个数
+      let slidesEls = swiperEl.getElementsByClassName('slide');
+
+      // 2.保存个数
       this.slideCount = slidesEls.length;
-      //大于1在前后分别添加slide
+
+      // 3.如果大于1个, 那么在前后分别添加一个slide
       if (this.slideCount > 1) {
-        // cloneNode()复制
-        let cloneFirst = slidesEls[0].cloneNode(true)
-        let cloneLast = slidesEls[this.slideCount - 1].cloneNode(true)
-        // insertBefore()向列表中插入一个项目 把一个列表项从一个列表移动到另一个中
-        swiperEl.insertBefore(cloneLast, slidesEls[0])
-        //后加节点
-        swiperEl.appendChild(cloneFirst)
+        let cloneFirst = slidesEls[0].cloneNode(true);
+        let cloneLast = slidesEls[this.slideCount - 1].cloneNode(true);
+        swiperEl.insertBefore(cloneLast, slidesEls[0]);
+        swiperEl.appendChild(cloneFirst);
         this.totalWidth = swiperEl.offsetWidth;
+        this.swiperStyle = swiperEl.style;
       }
-      //显示第一个
-      this.setTransform(-this.totalWidth)
+
+      // 4.让swiper元素, 显示第一个(目前是显示前面添加的最后一个元素)
+      this.setTransform(-this.totalWidth);
     },
-    // 拖动事件处理
+
+    /**
+     * 拖动事件的处理
+     */
     touchStart: function (e) {
-      if (this.scrolling) return
-      this.startTimer()
-      this.startx = e.touches[0].pageX;
+      // 1.如果正在滚动, 不可以拖动
+      if (this.scrolling) return;
+
+      // 2.停止定时器
+      this.stopTimer();
+
+      // 3.保存开始滚动的位置
+      this.startX = e.touches[0].pageX;
     },
-    touchMove: function () {
-      this.currentX = this.currentX - this.startx
-      this.distance = this.currentX = -this.startx;
-      let currentPosition = -this.currentIndex * this.totalWidth
-      let moveDisance = this.distance + currentPosition;
-      //设置当前位置
-      this.setTransform(moveDisance)
+
+    touchMove: function (e) {
+      // 1.计算出用户拖动的距离
+      this.currentX = e.touches[0].pageX;
+      this.distance = this.currentX - this.startX;
+      let currentPosition = -this.currentIndex * this.totalWidth;
+      let moveDistance = this.distance + currentPosition;
+
+      // 2.设置当前的位置
+      this.setTransform(moveDistance);
     },
+
     touchEnd: function () {
-      // abs获取绝对值
-      // 获取移动距离
-      let currentMove = Math.abs(this.distance)
-      // 判断最终距离
+      // 1.获取移动的距离
+      let currentMove = Math.abs(this.distance);
+
+      // 2.判断最终的距离
       if (this.distance === 0) {
         return
-      } else if (this.distance > 0 && currentMove > this.totalWidth * this.moveRatio) {
+      } else if (this.distance > 0 && currentMove > this.totalWidth * this.moveRatio) { // 右边移动超过0.5
         this.currentIndex--
-      } else if (this.distance < 0 && currentMove > this.totalWidth * this.moveRatio) {
+      } else if (this.distance < 0 && currentMove > this.totalWidth * this.moveRatio) { // 向左移动超过0.5
         this.currentIndex++
       }
-      // 移动到正确位置
-      this.scroolContent(-this.currentIndex * this.totalWidth);
-      // 定时器重开
+
+      // 3.移动到正确的位置
+      this.scrollContent(-this.currentIndex * this.totalWidth);
+
+      // 4.移动完成后重新开启定时器
       this.startTimer();
     },
-    // 控制上下个
-    pervious: function () {
+
+    /**
+     * 控制上一个, 下一个
+     */
+    previous: function () {
       this.changeItem(-1);
     },
+
     next: function () {
       this.changeItem(1);
     },
+
     changeItem: function (num) {
+      // 1.移除定时器
       this.stopTimer();
-      this.currentIndex += num
-      this.scroolContent(-this.currentIndex * this.totalWidth)
+
+      // 2.修改index和位置
+      this.currentIndex += num;
+      this.scrollContent(-this.currentIndex * this.totalWidth);
+
+      // 3.添加定时器
       this.startTimer();
     }
   }
-
-
 }
+
 </script>
 
 <style scoped>
@@ -193,7 +237,7 @@ export default {
   background-color: #fff;
   line-height: 8px;
   text-align: center;
-  font-style: 12px;
+  font-size: 12px;
   margin: 0 5px;
 }
 
