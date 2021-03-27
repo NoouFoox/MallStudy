067e0f2f59710d61b03af28c421b6f181a5864e3
nougat233 <nougat_hx@qq.com>
2021-03-27 11:31:18 +0800

    updata

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,10 +1,11 @@
 <template>
-  <div>
+  <div id="home">
     <navbar class="home-nav">
       <div slot="center">首页-购物街</div>
     </navbar>
     <HomeSwiper :banners="banners"/>
     <NFHomeReView :recommended="recommended"/>
+    <feture/>
   </div>
 </template>
 
@@ -14,16 +15,19 @@ import Navbar from "@/components/common/navbar/Navbar";
 import {getHomeDtat} from "@/network/home";
 import HomeSwiper from "./childComps/HomeSwiper";
 import NFHomeReView from "@/views/Home/childComps/NFHomeReView";
+import Feture from "@/views/Home/childComps/Feture";
+
 export default {
   name: "home",
   data() {
     return {
-      recommended:[],
+      recommended: [],
       result: null,
       banners: []
     }
   },
   components: {
+    Feture,
     NFHomeReView,
     HomeSwiper,
     Navbar,
@@ -39,8 +43,16 @@ export default {
 </script>
 
 <style scoped>
+#home{
+  padding-top: 43px;
+}
 .home-nav {
   background: #FF699C;
   color: white;
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  z-index: 9;
 }
 </style>
---
+++ src/views/Home/childComps/Feture.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="feture">
+    <a href="https://noou.club">
+      <!--.....-->
+      <img src="../../../../src/assets/img/a.png" class="feture-img">
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Feture"
+}
+</script>
+
+<style scoped>
+.feture-img{
+  width: 100%;
+  height: 40rem;
+  border-bottom: 8px solid #eee;
+}
+</style>
